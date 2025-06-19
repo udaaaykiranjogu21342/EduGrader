80c2dfff9cb371a8e6c69b3832da864a2c55aef8
udaaaykiranjogu21342 <udayjogu9@gmail.com>
2025-06-19 23:16:08 +0530

    first commit

--- sample.py
+++
@@ -1,8 +0,0 @@
-import pymongo
-
-try:
-    client = pymongo.MongoClient("mongodb+srv://udaykiranjogu2005:udaykiran@2005@edugrader.dog6lyc.mongodb.net/?retryWrites=true&w=majority&appName=EduGrader")
-    client.server_info()  # Forces connection on a request as the connect=True parameter of MongoClient seems not to work here
-    print("Connected to MongoDB successfully!")
-except Exception as e:
-    print("Failed to connect:", e)
